5ec3bc66040bdce9b813f72d80b131369acbb384
Dennis Hilgert <d.hilgert@laserhub.com>
2023-12-27 11:25:58 +0100

    Add cleanup

--- get-firecracker.sh
+++ get-firecracker.sh
@@ -7,4 +7,7 @@ curl -L ${release_url}/download/${latest}/firecracker-${latest}-${ARCH}.tgz \
 | tar -xz
 
 # Rename the binary to "firecracker"
-mv release-${latest}-$(uname -m)/firecracker-${latest}-${ARCH} firecracker
+mv release-${latest}-${ARCH}/firecracker-${latest}-${ARCH} firecracker
+
+# Cleanup
+rm -R release-${latest}-${ARCH}
