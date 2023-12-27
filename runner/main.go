bc6f685ed457fbd21fc797ea6ddd497478baa752
Dennis Hilgert <d.hilgert@laserhub.com>
2023-12-27 14:32:25 +0100

    Add signal handlers

--- runner/main.go
+++ runner/main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"math/rand"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"syscall"
 
 	"github.com/firecracker-microvm/firecracker-go-sdk"
 	"github.com/firecracker-microvm/firecracker-go-sdk/client/models"
@@ -15,12 +17,21 @@ import (
 var random = rand.New(rand.NewSource(1))
 
 func randomString(length int) string {
-	const allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	b := make([]byte, length)
 	for i := 0; i < length; i++ {
 		b[i] = allowed[random.Intn(len(allowed))]
 	}
 	return string(b)
+}
+
+func main() {
+	log.Print("Starting vm ...")
+	ctx, cancel := context.WithCancel(context.Background())
+	bootVm(ctx, "../vmlinux-5.10.204", "../ubuntu-22.04.ext4", "../firecracker")
+	defer cancel()
+
+	installSignalHandlers()
 }
 
 func bootVm(ctx context.Context, kernel string, image string, firecrackerBinary string) {
@@ -44,9 +55,10 @@ func bootVm(ctx context.Context, kernel string, image string, firecrackerBinary 
 		KernelArgs: "console=tty0 reboot=k panic=1 acpi=off pci=off i8042.noaux i8042.nomux i8042.nopnp i8042.dumbkbd nomodules",
 		Drives:     devices,
 		MachineCfg: models.MachineConfiguration{
-			VcpuCount:   firecracker.Int64(2),
-			CPUTemplate: models.CPUTemplateC3,
-			MemSizeMib:  firecracker.Int64(1024),
+			VcpuCount:       firecracker.Int64(2),
+			MemSizeMib:      firecracker.Int64(1024),
+			Smt:             firecracker.Bool(true),
+			TrackDirtyPages: *firecracker.Bool(false),
 		},
 		NetworkInterfaces: []firecracker.NetworkInterface{
 			{
@@ -72,16 +84,12 @@ func bootVm(ctx context.Context, kernel string, image string, firecrackerBinary 
 	machine, err := firecracker.NewMachine(vmmCtx, fcCfg, machineOpts...)
 	if err != nil {
 		fmt.Printf("Failed to start the vm: %+v\n", err)
+		vmmCancel()
 		os.Exit(1)
 	}
 
 	if err := machine.Start(vmmCtx); err != nil {
-		fmt.Printf("Failed to start the vm: %+v\n", err)
-		os.Exit(1)
-	}
-	defer machine.StopVMM()
-
-	if err := machine.Wait(vmmCtx); err != nil {
+		vmmCancel()
 		fmt.Printf("Failed to start the vm: %+v\n", err)
 		os.Exit(1)
 	}
@@ -89,7 +97,22 @@ func bootVm(ctx context.Context, kernel string, image string, firecrackerBinary 
 	log.Print("Machine was started")
 }
 
-func main() {
-	log.Print("Starting vm ...")
-	bootVm(context.Background(), "../vmlinux-5.10.204", "../ubuntu-22.04.ext4", "../firecracker")
+func installSignalHandlers() {
+	go func() {
+		// Clear some default handlers installed by the firecracker SDK:
+		signal.Reset(os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+
+		for {
+			switch s := <-c; {
+			case s == syscall.SIGTERM || s == os.Interrupt:
+				log.Printf("Caught signal: %s, requesting clean shutdown", s.String())
+				os.Exit(0)
+			case s == syscall.SIGQUIT:
+				log.Printf("Caught signal: %s, forcing shutdown", s.String())
+				os.Exit(0)
+			}
+		}
+	}()
 }
