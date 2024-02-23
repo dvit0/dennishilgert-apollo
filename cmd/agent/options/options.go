3726eff15fe9d216c7fa11cdf42c54f3ba30789a
dennis <dennis@hilgerts.de>
2024-02-23 13:19:53 +0000

    add api port option

--- cmd/agent/options/options.go
+++ cmd/agent/options/options.go
@@ -1,6 +1,8 @@
 package options
 
 import (
+	"flag"
+
 	"github.com/dennishilgert/apollo/pkg/logger"
 	"github.com/spf13/pflag"
 )
@@ -15,7 +17,7 @@ func New(origArgs []string) *Options {
 
 	// We are using pflag to parse the CLI flags
 	// pflag is a drop-in replacement for the standard library's "flag" package, however…
-	// There's one key difference: with the stdlib's "flag" package, there are no short-hand options so options can be defined with a single slash (such as "daprd -mode").
+	// There's one key difference: with the stdlib's "flag" package, there are no short-hand options so options can be defined with a single slash.
 	// With pflag, single slashes are reserved for shorthands.
 	// So, we are doing this thing where we iterate through all args and double-up the slash if it's single
 	// This works *as long as* we don't start using shorthand flags (which haven't been in use so far).
@@ -32,7 +34,10 @@ func New(origArgs []string) *Options {
 	fs := pflag.NewFlagSet("agent", pflag.ExitOnError)
 	fs.SortFlags = true
 
-	fs.IntVar(&opts.ApiPort, "api-port", 80, "the port used for the api server")
+	fs.IntVar(&opts.ApiPort, "api-port", 50051, "the port used for the api server")
+
+	opts.Logger = logger.DefaultOptions()
+	opts.Logger.AttachCmdFlags(flag.StringVar, flag.BoolVar)
 
 	// ignore errors; pflag is set for ExitOnError
 	_ = fs.Parse(args)
